15a00858f2125200cc895f6fefb65627425322f1
rojo-jojo <40956690+rojo-jojo@users.noreply.github.com>
2024-07-29 17:56:37 +0530

    lc 143

---
+++ leetcode/143_reorder_list.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        # Step 1 find the mid point
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        # Step 2 reverse the second half of the ll
+        second = slow.next
+        slow.next = None
+        prev = None
+        while second:
+            nxt = second.next
+            second.next = prev
+            prev = second
+            second = nxt
+        
+
+        # Step 3 merge the two LLs
+        first, second = head, prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2
